9c69f68d0c782d34d1aff8524816a459c242aa28
apaezp <andrea.paez86@gmail.com>
2023-02-25 20:33:08 -0300

    some changes regarding styles

--- src/components/data.js
+++ src/components/data.js
@@ -6,7 +6,7 @@ const reviews = [
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
       text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do einim id est laborum.",
     },
     {
       id: 2,
@@ -15,7 +15,7 @@ const reviews = [
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
       text:
-        'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+        'Lorem ipsum dolor sit amet, consect.',
     },
     {
       id: 3,
--- src/components/pages/VetProfile/VetHome.js
+++ src/components/pages/VetProfile/VetHome.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Button } from '../../Button';
 import Search from '../Search'
 import Footer from "../../Footer.js"
+import TopVetReviews from "../../TopVetReviews"
+import "./VetHome.css"
 
 
 function VetHome() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
-
-
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
@@ -18,21 +17,25 @@ function VetHome() {
       setButton(true);
     }
   };
+ 
 
   useEffect(() => {
     showButton();
   }, []);
 
   window.addEventListener('resize', showButton);
+
+
   return (
     <>
-    <div >VetHome</div>
+    <div className='VetHomeContainer'> 
+
+    <Search />     
     <Link to='/pages/VetProfile/VetPublicProfile' >
           {button && <button type="button" className="btn btn-dark" onClick={click}>Ver perfil</button>}
     </Link>
-
-    <Search /> 
     <Footer />
+    </div>
     </>
   )
 }
