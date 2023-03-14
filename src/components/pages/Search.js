7cbabe25077fadfa106d00c7920d5897cf8678b3
apaezp <andrea.paez86@gmail.com>
2023-03-13 22:20:44 -0300

    Connecting search with DB

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { GiMagnifyingGlass } from 'react-icons/gi'
-import people from '../data'
+import axios from 'axios'
 import "./Search.css";
 
 
@@ -8,55 +8,55 @@ function Search() {
   const [search, setSearch] = useState('')    
   const [searchResults, setSearchResults] = useState([])    
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault()
-
-    const filteredResults = people.filter(item => {
-        return item.name.toLowerCase().includes(search.toLowerCase())
-    } )        
-
+  
     if(search === ''){
         alert('Please enter a name');
         return
-    } else if (filteredResults.length === 0){
-        alert('No results found');
-        return
     } else {
-        setSearchResults(filteredResults)
+      const urlServer = "https://backendgopaw-production.up.railway.app";
+      const endpoint = `/veterinarys?name=${search}`;
+      try {
+        const { data } = await axios.get(urlServer + endpoint);
+        setSearchResults(data);
+      } catch ({ response: { data: message } }) {
+        alert(message + " 🙁");
+        console.log(message);
+      }
     }
-
+  
     setSearch('');
-
-};
-
-return (
-  <>
-  <div className="search-card-container">  
-    <div className="input-name">
-      <input className="search-input" type="text" placeholder="Busca tu veterinario" value={search} onChange={(e) => setSearch(e.target.value)} />
-     
-      <button className="btn-search" onClick={handleSearch}>
-        {" "}
-        <GiMagnifyingGlass clasName="magnifying-glass-icon "/>{" "}
-      </button>
-    </div>
-    {searchResults.map(item => (
-      <div className='cardVeterinary' key={item.id}>
-      <div className='imgCardVet'>
-      <img className='cardImgVet'src={item.image} alt={item.name}/>
+  };
+  
+  const searchResult = searchResults.find(({ veterinary_name }) => veterinary_name.toLowerCase().includes(search.toLowerCase()))
+  
+  return (
+    <>
+      <div className="search-card-container">  
+        <div className="input-name">
+          <input className="search-input" type="text" placeholder="Busca tu veterinario" value={search} onChange={(e) => setSearch(e.target.value)} />
+         
+          <button className="btn-search" onClick={handleSearch}>
+            {" "}
+            <GiMagnifyingGlass clasName="magnifying-glass-icon "/>{" "}
+          </button>
+        </div>
+        {searchResult && (
+          <div className='cardVeterinary' key={searchResult.id}>
+            <div className='imgCardVet'>
+              <img className='cardImgVet' src={searchResult.image} alt={searchResult.veterinary_name} />
+            </div>
+            <div className='cardInfoVet'>
+              <h3 className='nameVet'>{searchResult.veterinary_name}</h3>
+              <h4 className='dateReview'>{searchResult.phone}</h4>
+              <h4 className='text'>{/* review */}</h4>
+            </div>
+          </div>
+        )}
       </div>
-      <div className='cardInfoVet'>
-        <h3 className='nameVet'>{item.name}</h3>
-        <h4 className='dateReview'>{item.date}</h4>
-        <h4 className='text'>{item.text}</h4>
-      </div>
-  </div>
-      ))}
-  </div>
-  </>
-)
-}     
-
-
+    </>
+  ); 
+}
 
 export default Search;
