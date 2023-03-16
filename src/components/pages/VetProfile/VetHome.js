9396965b35a72d3474f2020537ef4560b857b18e
apaezp <andrea.paez86@gmail.com>
2023-03-15 22:24:14 -0300

    fixing styles

--- src/components/pages/VetProfile/VetHome.js
+++ src/components/pages/VetProfile/VetHome.js
@@ -1,30 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Search from "../Search";
 import Footer from "../../Footer.js";
 import "./VetHome.css";
+import Video from "../../assets/video/vetHome.mp4";
 
 function VetHome() {
  
   return (
     <>
-      <div className="vetHomeContainer">
-        <div className="vetTitle"> 
-        <h1>Bienvenido a la pagina de veterinarios, aqui podras encontrar a tu veterinario favorito</h1>
-        <div className="vetHomeButtons"> 
-        {/* <Link to="/pages/VetProfile/VetPublicProfile">
-          {button && (
-            <button type="button" className="btn btn-dark" onClick={click}>
-              Ver perfil
-            </button>
-          )}
-        </Link> */}
-         <Search />     
+      <div className="vetHomeContainer"> 
+      <video src={Video} autoPlay loop muted />
+      <div>
+         <h1 className="vetTitle">Bienvenido a la pagina de veterinarios, aqui podras encontrar a tu veterinario favorito</h1>
+       </div>        
+        <div className="searchContainer">        
+         <Search /> 
         </div>
-        
-        </div>
-      </div>
-      
+        </div>       
       <Footer />
     </>
   );
