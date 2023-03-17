e92f43f26e897c219560a88f8406de4c52164db2
MatiBriceño <bvmatiasnicolas@gmail.com>
2023-03-16 23:32:05 -0400

    Perfil citas y perfilReseñas

--- src/components/PerfilReseña.js
+++ src/components/PerfilReseña.js
@@ -1,6 +1,42 @@
-import React from 'react'
+
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../GlobalStates";
+
 
 export const PerfilReseña = () => {
+
+  const [authState] = useContext(AuthContext);
+  const [reviewData, setReviewData] = useState([]);
+  const { id } = authState;
+
+  console.log(id)
+
+  const viewReview = async (id) => {
+    const urlServerGET = "http://localhost:8080";
+    const endpointGET = `/review/${id}`
+
+    
+      const response = await axios.get(urlServerGET + endpointGET, {
+        params: { id },
+      });
+
+    if (response && response.data) {
+      console.log(response);
+      setReviewData(response.data);
+      
+    } else {
+      alert("No se pudo obtener las reseñas del veterinario.");
+    }
+  };
+ 
+
+  useEffect(() => {
+    viewReview(id);
+    console.log(reviewData);
+  }, [])
+  
+
   return (
 
     <div className="tab-pane" id="messages">
@@ -10,37 +46,19 @@ export const PerfilReseña = () => {
      <i className="icon-info"></i>
     </div>
     <div className="alert-message">
-      <span><strong>Info!</strong> Lorem Ipsum is simply dummy text.</span>
+      <span><strong>Info!</strong> ¡Mira que opina la gente de ti!</span>
     </div>
   </div>
     <table className="table table-hover table-striped">
-        <tbody>                                    
-            <tr>
-                <td>
-                   <span className="float-right font-weight-bold">3 hrs ago</span> Here is your a link to the latest summary report from the..
-                </td>
-            </tr>
-            <tr>
-                <td>
-                   <span className="float-right font-weight-bold">Yesterday</span> There has been a request on your account since that was..
-                </td>
-            </tr>
-            <tr>
-                <td>
-                   <span className="float-right font-weight-bold">9/10</span> Porttitor vitae ultrices quis, dapibus id dolor. Morbi venenatis lacinia rhoncus. 
-                </td>
-            </tr>
-            <tr>
-                <td>
-                   <span className="float-right font-weight-bold">9/4</span> Vestibulum tincidunt ullamcorper eros eget luctus. 
-                </td>
-            </tr>
-            <tr>
-                <td>
-                   <span className="float-right font-weight-bold">9/4</span> Maxamillion ais the fix for tibulum tincidunt ullamcorper eros. 
-                </td>
-            </tr>
-        </tbody> 
+    <tbody>
+  {reviewData.map((review) => (
+    <tr key={review.id}>
+      <td>
+        <span className="float-right font-weight-bold">{review.date}</span> {review.content}
+      </td>
+    </tr>
+  ))}
+</tbody>
     </table>
 </div>
   )
